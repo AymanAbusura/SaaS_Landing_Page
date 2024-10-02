aee0f56cdf5bddc8b174848af1fa3c2a3e7b6d2e
Ayman Abusura <aymansalameh1996@hotmail.com>
2024-10-02 11:03:20 +0300

    Done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom"
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
+const rootNode = document.getElementById('root');
+// eslint-disable-next-line react/no-deprecated
+ReactDOM.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>, rootNode);
